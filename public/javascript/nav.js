547d07e72ae76ea05ff23f815a8a60effb93c197
Bailey Lockheart <baileylockheart@gmail.com>
2025-01-05 01:05:57 -0800

    Add files via upload

---
+++ public/javascript/nav.js
@@ -0,0 +1,12 @@
+function abtMe() {
+    document.getElementById("myDropdownMe").classList.toggle("show");
+    }
+function abtSite() {
+    document.getElementById("myDropdownSite").classList.toggle("show");
+    }
+function forYou() {
+    document.getElementById("myDropdownYou").classList.toggle("show");
+    }
+function other() {
+    document.getElementById("myDropdownOther").classList.toggle("show");
+    }
---
+++ public/javascript/watchlist.js
@@ -0,0 +1,114 @@
+document.getElementById("watchlist").innerHTML = `
+<style>
+.awentry{
+display:inline-block;
+background-color: var(--accent);
+color: var(--text);
+font-size:0.8rem;
+text-align:center;
+padding: 0.2rem 0.5rem;
+border: 2px solid var(--text);
+width:18rem;
+margin: 5px 0px;}
+.awentry:hover{
+    transition: all .2s ease-out;
+    transform: scale(1.02); 
+}
+.awpic{
+    padding-top: 0.1rem;
+    width: 100%
+}
+.awentry p, .awentry h3{
+text-align:center;line-height:0.5rem}
+</style>
+<div class="awentry">
+    <h3>My Hero Academia &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/bnha.jpg">
+    <p>Need to Restart || S6 E3</p>
+</div>
+<div class="awentry">
+    <h3>Haikyuu!! &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/haikyuu.jpg">
+    <p>Finished/Caught Up || S4 E25</p>
+</div>
+<div class="awentry">
+    <h3>Yuri!!! on Ice &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/yoi.jpg">
+    <p>Finished/Caught Up || S1 E12</p>
+</div>
+<div class="awentry">
+    <h3>Sarazanmai &star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/sarazanmai.jpg">
+    <p>Finished/Caught Up || S1 E11</p>
+</div>
+<div class="awentry">
+    <h3>Cells at Work &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/cellsatwork.jpeg">
+    <p>Finished/Caught Up || S2 E8</p>
+</div>
+<div class="awentry">
+    <h3>A Silent Voice &star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/silentvoice.jpg">
+    <p>Finished Movie</p>
+</div>
+<div class="awentry">
+    <h3>Demon Slayer &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/demonslayer.jpg">
+    <p>Haven't Finished || S3 E11</p>
+</div>
+<div class="awentry">
+    <h3>Sk8 The Infinity &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/sk8.jpg">
+    <p>Finished/Caught Up || S1 E12</p>
+</div>
+<div class="awentry">
+    <h3>Toradora &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/toradora.jpg">
+    <p>Need to Restart</p>
+</div>
+<div class="awentry">
+    <h3>If It's for My Daughter... &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/demonlord.jpeg">
+    <p>Finished/Caught Up || S1 E12</p>
+</div>
+<div class="awentry">
+    <h3>Jujutsu Kaisen &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/jjk.jpg">
+    <p>Haven't Finished || S1 E21</p>
+</div>
+<div class="awentry">
+    <h3>Death Note &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/deathnote.jpeg">
+    <p>Finished/Caught Up || S1 E37</p>
+</div>
+<div class="awentry">
+    <h3>Kotaro Lives Alone &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/kotaro.jpg">
+    <p>Finished/Caught Up || S1 E10</p>
+</div>
+<div class="awentry">
+    <h3>Food Wars &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/foodwars.jpeg">
+    <p>Finished/Caught Up || S5 E13</p>
+</div>
+<div class="awentry">
+    <h3>Given &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/given.jpeg">
+    <p>Finished/Caught Up || S1 E11</p>
+</div>
+<div class="awentry">
+    <h3>Danganronpa: The Animation &star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/danganropa.jpeg">
+    <p>Finished/Caught Up || S1 E13</p>
+</div>
+<div class="awentry">
+    <h3>Banana Fish &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/bananafish.jpg">
+    <p>Finished/Caught Up || S1 E24</p>
+</div>
+<div class="awentry">
+    <h3>WAVE!! -Let's go surfing!!- &star;&star;&star;&star;&star;</h3>
+    <img class="awpic" src="https://file.garden/ZrZSgsrYfQXsO7QH/watchlist/wave.jpg">
+    <p>Finished/Caught Up || S1 E12</p>
+</div>
+`;
